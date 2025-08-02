d45db9ca4404daac46e30f73320ddce877c2ad49
ggomez0 <gaspargomez2000@outlook.com>
2025-08-01 21:13:17 -0300

    Update index, skip nulls values

--- src/index.py
+++ src/index.py
@@ -7,8 +7,7 @@ def get_request(url):
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        return data
+        return response.json()
     else:
         print(f'Error al obtener el JSON: {response.status_code}')
 
@@ -36,18 +35,24 @@ def main():
                 f'https://www.flylevel.com/nwe/flights/api/calendar/?triptype=RT&origin=EZE&destination=BCN&month={month}&year={year_data["year"]}&currencyCode=USD'
             )
             if response and 'data' in response and 'dayPrices' in response['data']:
-                all_day_prices.extend(response['data']['dayPrices'])
+                valid_prices = [p for p in response['data']['dayPrices'] if p['price'] is not None]
+                all_day_prices.extend(valid_prices)
             else:
                 print(f"Datos no encontrados para el mes {month} del año {year_data['year']}")
 
-    top_10 = sorted(all_day_prices, key=lambda x: x['price'])[:1]
-    
-    print(json.dumps({'dayPrices': top_10}, indent=2))
+    if all_day_prices:
+        top_10 = sorted(all_day_prices, key=lambda x: x['price'])[:1]
 
-    for price_info in top_10:
-        if price_info['price'] < min_price:
-            message = f"¡Precio bajo encontrado! {price_info['price']} USD para la fecha {price_info['date']}" \
-                      f"https://www.flylevel.com/Flight/Select?culture=es-ES&triptype=OW&o1=EZE&d1=BCN&dd1={price_info['date']}&ADT=1&CHD=0&INL=0&r=false&mm=false&forcedCurrency=USD&forcedCulture=es-ES&newecom=true&currency=USD"
-            send_telegram_message(message, telegram_token, telegram_chat_id)
+        print(json.dumps({'dayPrices': top_10}, indent=2))
+
+        for price_info in top_10:
+            if price_info['price'] < min_price:
+                message = (
+                    f"¡Precio bajo encontrado! {price_info['price']} USD para la fecha {price_info['date']} "
+                    f"https://www.flylevel.com/Flight/Select?culture=es-ES&triptype=OW&o1=EZE&d1=BCN&dd1={price_info['date']}&ADT=1&CHD=0&INL=0&r=false&mm=false&forcedCurrency=USD&forcedCulture=es-ES&newecom=true&currency=USD"
+                )
+                send_telegram_message(message, telegram_token, telegram_chat_id)
+    else:
+        print("No se encontraron precios válidos")
 
 main()
